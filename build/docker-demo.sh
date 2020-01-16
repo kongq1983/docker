2659415db3ff687593cd7861abbebd662c93a32f
king <king@qq.com>
2020-01-16 17:53:31 +0800

    mvn clean -f ../pom.xml

--- build/docker-demo.sh
+++ build/docker-demo.sh
@@ -1,11 +1,11 @@
 
 # 先清空
-mvn clean
+mvn clean -f ../pom.xml
 # 先执行common打包
-mvn package  -f ../common/pom.xml
+mvn package -f ../common/pom.xml
 
 # 然后在对docker打包 这里依赖common包
-mvn package  -f ../pom.xml
+mvn package -f ../pom.xml
 
 
 docker build -f ProductServiceDockerfile .
